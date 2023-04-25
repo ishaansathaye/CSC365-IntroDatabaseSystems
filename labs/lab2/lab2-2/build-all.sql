65724e6a55282dbb570f20386929b779d86bb782
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2023-04-25 03:15:26 -0700

    testing

--- labs/lab2/lab2-2/build-all.sql
+++ labs/lab2/lab2-2/build-all.sql
@@ -14,12 +14,12 @@ source AIRLINES/AIRLINES-build-flights.sql
 
 -- BAKERY
 -- set echo on
--- source BAKERY/BAKERY-setup.sql
--- -- set echo off
--- source BAKERY/BAKERY-build-goods.sql
--- source BAKERY/BAKERY-build-customers.sql
--- source BAKERY/BAKERY-build-receipts.sql
--- source BAKERY/BAKERY-build-items.sql
+source BAKERY/BAKERY-setup.sql
+-- set echo off
+source BAKERY/BAKERY-build-goods.sql
+source BAKERY/BAKERY-build-customers.sql
+source BAKERY/BAKERY-build-receipts.sql
+source BAKERY/BAKERY-build-items.sql
 
 
 -- -- CARS
