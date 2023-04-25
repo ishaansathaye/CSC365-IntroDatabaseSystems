2bc97eab553c7024d3b9e27cbdce0e8ef0c9691c
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2023-04-25 03:17:04 -0700

    testing

--- labs/lab2/lab2-2/build-all.sql
+++ labs/lab2/lab2-2/build-all.sql
@@ -5,11 +5,11 @@
 -- AIRLINES
 
 -- set echo on
-source AIRLINES/AIRLINES-setup.sql
--- set echo off
-source AIRLINES/AIRLINES-build-airports100.sql
-source AIRLINES/AIRLINES-build-airlines.sql
-source AIRLINES/AIRLINES-build-flights.sql
+-- source AIRLINES/AIRLINES-setup.sql
+-- -- set echo off
+-- source AIRLINES/AIRLINES-build-airports100.sql
+-- source AIRLINES/AIRLINES-build-airlines.sql
+-- source AIRLINES/AIRLINES-build-flights.sql
 
 
 -- BAKERY
